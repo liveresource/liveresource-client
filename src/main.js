986e5cb3cdf1358309e6d6ad2e27959a5e89a025
harmony7 <harmony7@pex2.jp>
2015-08-07 16:22:03 +0900

    Had forgotten to add changes engine unit to the engine

--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ var ChangesEngineUnit = require('changes/ChangesEngineUnit');
 
 var engine = Engine();
 engine.addEngineUnit(new ValueEngineUnit());
+engine.addEngineUnit(new ChangesEngineUnit());
 
 module.exports = require('engine-components/LiveResource');
